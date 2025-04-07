7028f033abda2b2015b93bdde54e945d0a63db8b
Nour Harrak <n01154202@humber.ca>
2025-04-06 22:21:29 -0400

    student object created

---
+++ script.js
@@ -0,0 +1,16 @@
+//Part 1 - Understanding and Creating Objects
+
+let student = {
+    name: "Nour Harrak",
+    age: 28,
+    isEnrolled: true,
+    courses: ['JavaScript Fundamentals', 'Web Programming and Design', 'Operating Systems', 'Algorithms and Problem Solving', 'Database Programming', 'Technical Workplace Writing Skills'],
+    details: function() {
+        console.log("The Student's Name is: " + this.name);
+        console.log("The Student's Name is: " + this.age);
+        console.log("The Student is Enrolled?: " + this.isEnrolled);
+        console.log("The Student's is Currently Taking the Following Courses: " + this.courses);
+    }
+};
+
+student.details();
